1aafdea645047859f7519c63865a3c028f9a1ebe
daili <daiybh@gmail.com>
2024-06-21 22:29:49 +0800

    asyncio  mutil tasks

--- testtools/client.py
+++ testtools/client.py
@@ -1,18 +1,28 @@
 import websockets
 import asyncio
-async def main():
-    async with websockets.connect('ws://localhost:9002') as websocket:
-        
-        await websocket.send(f'Register')
-        while True:            
-            message = await websocket.recv()
-            print(message)
 
 import time
-while True:
-    try:
-        asyncio.run(main())
-    except Exception as ex:
-        print('error',ex)
-        pass
-    time.sleep(1)
+
+async def connectWS(index):
+    while True:
+        try:
+            async with websockets.connect('ws://localhost:9002') as websocket:
+                await websocket.send(f'Register')
+                while True:
+                    message = await websocket.recv()    
+                    print(f"{index} >> {message}")
+        except Exception as ex:
+            print(f'{index} >> error',ex)
+        await asyncio.sleep(1)
+
+
+
+async def main():
+    tasks = []
+    for i in range(10):
+        tasks += [asyncio.create_task(connectWS(i))]
+        
+    await asyncio.gather(*tasks)
+    print("主任务完成")
+
+asyncio.run(main())
