e9abb5a480915bbe43c6f3b56d574eb2bdceb041
daili <daiybh@gmail.com>
2024-06-26 09:51:45 +0800

    disable all websocketpp log

--- src/simple_broadcast_server.cpp
+++ src/simple_broadcast_server.cpp
@@ -21,7 +21,7 @@ public:
 	broadcast_server()
 	{
 		m_server.init_asio();
-
+		m_server.clear_access_channels(websocketpp::log::alevel::all);
 		m_server.set_open_handler(bind(&broadcast_server::on_open, this, ::_1));
 		m_server.set_close_handler(bind(&broadcast_server::on_close, this, ::_1));
 		m_server.set_message_handler(bind(&broadcast_server::on_message, this, ::_1, ::_2));
